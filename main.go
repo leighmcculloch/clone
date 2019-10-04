4fa3278e5888a0825c9b095f4aa02a6e21bbd336
Leigh McCulloch <leigh@mcchouse.com>
2019-10-03 22:29:09 -0700

    Handle private repositories gracefully

--- main.go
+++ main.go
@@ -55,17 +55,31 @@ func main() {
 	repoPath := path.Join(username, repo)
 
 	httpsURL := fmt.Sprintf("https://github.com/%s", repoPath)
-	cloneCmd := exec.Command("git", "clone", httpsURL, target)
+	sshURL := fmt.Sprintf("git@github.com:%s", repoPath)
+
+	public := true
+	headResp, err := http.Head(httpsURL)
+	if err != nil {
+		color.Yellow("Warning: Unable to check if project is public/private: %s\n", err)
+		color.Yellow("Assuming repository is public.\n")
+	} else {
+		public = headResp.StatusCode == http.StatusOK
+	}
+
+	cloneURL := httpsURL
+	if !public {
+		cloneURL = sshURL
+	}
+	cloneCmd := exec.Command("git", "clone", cloneURL, target)
 	cloneCmd.Stdin = os.Stdin
 	cloneCmd.Stdout = os.Stdout
 	cloneCmd.Stderr = os.Stderr
-	color.Green("Cloning repo %s:\n", httpsURL)
+	color.Green("Cloning repository %s:\n", cloneURL)
 	if err := cloneCmd.Run(); err != nil {
 		color.Red("Error cloning: %s\n", err)
 		os.Exit(1)
 	}
 
-	sshURL := fmt.Sprintf("git@github.com:%s", repoPath)
 	setURLCmd := exec.Command("git", "remote", "set-url", "--add", "--push", "origin", sshURL)
 	setURLCmd.Dir = filepath.Join(".", target)
 	setURLCmd.Stdin = os.Stdin
@@ -76,46 +90,48 @@ func main() {
 		os.Exit(1)
 	}
 
-	apiURL := fmt.Sprintf("https://api.github.com/repos/%s", repoPath)
-	apiResp, err := http.Get(apiURL)
-	if err != nil {
-		color.Yellow("Warning: Unable to check if fork: %s\n", err)
-		os.Exit(0)
-	}
-	if apiResp.StatusCode != http.StatusOK {
-		color.Yellow("Warning: Unable to check if fork: %s\n", apiResp.Status)
-		os.Exit(0)
-	} else {
-		defer apiResp.Body.Close()
-		apiRespDec := struct {
-			Parent struct {
-				CloneURL string `json:"clone_url"`
-				SSHURL   string `json:"ssh_url"`
-			} `json:"parent"`
-		}{}
-		err := json.NewDecoder(apiResp.Body).Decode(&apiRespDec)
+	if public {
+		apiURL := fmt.Sprintf("https://api.github.com/repos/%s", repoPath)
+		apiResp, err := http.Get(apiURL)
 		if err != nil {
 			color.Yellow("Warning: Unable to check if fork: %s\n", err)
 			os.Exit(0)
 		}
-		if apiRespDec.Parent.SSHURL != "" {
-			setUpstreamCmd := exec.Command("git", "remote", "add", "upstream", apiRespDec.Parent.CloneURL)
-			setUpstreamCmd.Dir = filepath.Join(".", target)
-			setUpstreamCmd.Stdin = os.Stdin
-			setUpstreamCmd.Stdout = os.Stdout
-			setUpstreamCmd.Stderr = os.Stderr
-			if err := setUpstreamCmd.Run(); err != nil {
-				color.Red("Error setting upstream: %s\n", err)
-				os.Exit(1)
+		if apiResp.StatusCode != http.StatusOK {
+			color.Yellow("Warning: Unable to check if fork: %s\n", apiResp.Status)
+			os.Exit(0)
+		} else {
+			defer apiResp.Body.Close()
+			apiRespDec := struct {
+				Parent struct {
+					CloneURL string `json:"clone_url"`
+					SSHURL   string `json:"ssh_url"`
+				} `json:"parent"`
+			}{}
+			err := json.NewDecoder(apiResp.Body).Decode(&apiRespDec)
+			if err != nil {
+				color.Yellow("Warning: Unable to check if fork: %s\n", err)
+				os.Exit(0)
 			}
-			setUpstreamPushCmd := exec.Command("git", "remote", "set-url", "--add", "--push", "upstream", apiRespDec.Parent.SSHURL)
-			setUpstreamPushCmd.Dir = filepath.Join(".", target)
-			setUpstreamPushCmd.Stdin = os.Stdin
-			setUpstreamPushCmd.Stdout = os.Stdout
-			setUpstreamPushCmd.Stderr = os.Stderr
-			if err := setUpstreamPushCmd.Run(); err != nil {
-				color.Red("Error setting upstream: %s\n", err)
-				os.Exit(1)
+			if apiRespDec.Parent.SSHURL != "" {
+				setUpstreamCmd := exec.Command("git", "remote", "add", "upstream", apiRespDec.Parent.CloneURL)
+				setUpstreamCmd.Dir = filepath.Join(".", target)
+				setUpstreamCmd.Stdin = os.Stdin
+				setUpstreamCmd.Stdout = os.Stdout
+				setUpstreamCmd.Stderr = os.Stderr
+				if err := setUpstreamCmd.Run(); err != nil {
+					color.Red("Error setting upstream: %s\n", err)
+					os.Exit(1)
+				}
+				setUpstreamPushCmd := exec.Command("git", "remote", "set-url", "--add", "--push", "upstream", apiRespDec.Parent.SSHURL)
+				setUpstreamPushCmd.Dir = filepath.Join(".", target)
+				setUpstreamPushCmd.Stdin = os.Stdin
+				setUpstreamPushCmd.Stdout = os.Stdout
+				setUpstreamPushCmd.Stderr = os.Stderr
+				if err := setUpstreamPushCmd.Run(); err != nil {
+					color.Red("Error setting upstream: %s\n", err)
+					os.Exit(1)
+				}
 			}
 		}
 	}
